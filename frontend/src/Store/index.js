b4a4323cf77571d5ecc3017cb8dc4844de336e9d
Josh Weaver <jweaver911@gmail.com>
2017-10-19 00:22:05 -0500

    chore: remove logging

--- frontend/src/Store/index.js
+++ frontend/src/Store/index.js
@@ -2,20 +2,17 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "../Reducers";
 import thunk from "redux-thunk";
 
-const logger = store => next => action => {
-  console.group(action.type);
-  console.info("dispatching", action);
-  let result = next(action);
-  console.log("next state", store.getState());
-  console.groupEnd(action.type);
-  return result;
-};
+// const logger = store => next => action => {
+//   console.group(action.type);
+//   console.info("dispatching", action);
+//   let result = next(action);
+//   console.log("next state", store.getState());
+//   console.groupEnd(action.type);
+//   return result;
+// };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(thunk, logger))
-);
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
